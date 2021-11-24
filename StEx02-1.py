863341dbae628a09b92f075fde389e45875d8b4b
ryota MIYAZAKI <74343376+yudis9c@users.noreply.github.com>
2021-11-24 18:17:06 +0900

    Update StEx02-1.py

--- StEx02-1.py
+++ StEx02-1.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 
+namelist = ["price","advertising expenses","Number of negative reviews","Number of positive reviews"]
 for i in range(1,5):
   x = [dat[:,i]]
   y = [dat[:,0]]
@@ -15,3 +16,8 @@ for i in range(1,5):
               alpha = 0.8,
               linewidths = 0.5,
               edgecolors = 'black')
+  
+  plt.title("Relationship between" + namelist[i-1] + "and the number of sales")  # タイトル
+  plt.xlabel(namelist[i-1]) # 軸名
+  plt.ylabel("Number of sales") #軸名
+  plt.grid(True)      #グリッド線（True:引く、False:引かない）
