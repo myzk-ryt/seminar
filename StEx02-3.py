aec32ac24b48ea949f4e76f18bb5e61d3f388fef
ryota MIYAZAKI <74343376+yudis9c@users.noreply.github.com>
2021-11-28 12:51:08 +0900

    Update StEx02-3.py

--- StEx02-3.py
+++ StEx02-3.py
@@ -20,7 +20,19 @@ ModelLR.fit(x,y)
 
 print("切片付き回帰式A：y^ = ",round(ModelLR.coef_[0],3), "x1 + ", round(ModelLR.coef_[1],3), "x2 + ", round(ModelLR.coef_[2],3), "x3 + ", round(ModelLR.intercept_,3))
 
+#AのRMSE評価
+y_pred_01 = ModelLR.predict(x)
+Mse_01 = mean_squared_error(y,y_pred_01)
+Rmse_01 = np.sqrt(Mse_01)
+print("回帰式AのRMSE：", Rmse_01)
+
 ModelLR = LinearRegression(fit_intercept=False) # 線形回帰モデルのセット
 ModelLR.fit(x, y)
 
 print("切片なし回帰式B：y^ = ",round(ModelLR.coef_[0],3), "x1 + ", round(ModelLR.coef_[1],3), "x2 + ", round(ModelLR.coef_[2],3), "x3 + ", round(ModelLR.intercept_,3))
+
+#BのRMSE評価
+y_pred_02 = ModelLR.predict(x)
+Mse_02 = mean_squared_error(y,y_pred_02)
+Rmse_02 = np.sqrt(Mse_02)
+print("回帰式AのRMSE：", Rmse_02)
