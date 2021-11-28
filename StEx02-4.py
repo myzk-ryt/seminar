b4d09f6b95ee68fa5c4a188b00fcc121e6a16202
yudis9c <74343376+yudis9c@users.noreply.github.com>
2021-11-28 16:47:24 +0900

    Update StEx02-4.py

--- StEx02-4.py
+++ StEx02-4.py
@@ -16,3 +16,8 @@ yosoku = round(ModelLR.coef_[0] * dat[5,1] + ModelLR.coef_[1] * dat[5,3] + Model
 print("予測値：", yosoku)
 print("実測値：", dat[5,0])
 print("誤差：", round(yosoku - dat[5,0],3))
+
+#---出力結果---
+#予測値： 75.392
+#実測値： 60.0
+#誤差： 15.392
