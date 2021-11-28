55cb556dccdc32b4fabe22bc4d6ba5a8a368160a
yudis9c <74343376+yudis9c@users.noreply.github.com>
2021-11-28 16:46:37 +0900

    Update StEx02-4.py

--- StEx02-4.py
+++ StEx02-4.py
@@ -1,3 +1,17 @@
+y = dat[:,0]
+x1 = dat[:,1]
+x2 = dat[:,3]
+x3 = dat[:,4]
+
+#それぞれを一列の行列に直してxを生成
+x1 = np.reshape(x1,(20,1))
+x2 = np.reshape(x2,(20,1))
+x3 = np.reshape(x3,(20,1))
+x = np.concatenate([x1,x2,x3],1)
+
+ModelLR = LinearRegression()
+ModelLR.fit(x,y)
+
 yosoku = round(ModelLR.coef_[0] * dat[5,1] + ModelLR.coef_[1] * dat[5,3] + ModelLR.coef_[2] * dat[5,4] + ModelLR.intercept_,3)
 print("予測値：", yosoku)
 print("実測値：", dat[5,0])
