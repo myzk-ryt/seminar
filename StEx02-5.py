36893f9f21c875b5f4022e333c344ac385392666
yudis9c <74343376+yudis9c@users.noreply.github.com>
2021-12-02 12:11:35 +0900

    Update StEx02-5.py

--- StEx02-5.py
+++ StEx02-5.py
@@ -1,25 +1,25 @@
-#ネガティブな口コミの平均値を求める。
+#---ネガティブな口コミの平均値を求める。---
 M = np.mean(dat[:,3])
 
 H = []
 L = []
 
-#ネガティブな口コミが平均以上の製品の値段をH、以下の製品の値段をLに格納する。
+#---ネガティブな口コミが平均以上の製品の値段をH、以下の製品の値段をLに格納する。---
 for i in range(len(dat[:,0])):
   if dat[i,3] >= M:
     H.append(dat[i,0])
   else:
     L.append(dat[i,0])
 
-#H、Lの平均値を求める。
+#---H、Lの平均値を求める。---
 Have = sum(H) / len(H)
 Lave = sum(L) / len(L)
 
-#差を求める。
+#---差を求める。---
 difference = Lave - Have
 print("高批判製品群・低批判製品群それぞれの販売数の平均値の差は", difference, "でした。")
 
-#t検定を行い、p値を求める。
+#---t検定を行い、p値を求める。---
 Ttest = st.ttest_ind(H, L, equal_var=False)
 print("t検定のp値は、小数点第四位で丸めて", round(Ttest[1],4), "でした。")
 if Ttest[1] < 0.01:
